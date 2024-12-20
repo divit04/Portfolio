dcc969984e98bcd408907b97c0c5a32e38c1c6f9
divit04 <117028613+divit04@users.noreply.github.com>
2024-12-20 23:15:02 +0530

    script.js

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ window.addEventListener('scroll',()=>{
 //light mode and dark mode
 
 
+document.documentElement.classList.toggle(
+    'dark',
+    localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
 
 function toggleTheme(){
 
