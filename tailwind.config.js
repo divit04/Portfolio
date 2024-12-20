c476c24321112d7d68c74a432cae1c97a5ac842b
divit04 <117028613+divit04@users.noreply.github.com>
2024-12-20 23:03:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config={
+    extends:{
+        theme:{
+        gridTemplateColumn:{
+            'auto': 'repeat(auto-fit, minmax(200px,1fr))'
+        }, 
+        fontFamily:{
+            Outfit:["Outfit","sans-serif"],
+            Ovo:["Ovo","sans-serif"]
+        },
+        colors:{
+         darkTheme:"#11001F"
+        }
+    }
+    },
+  darkMode: 'selector'
+    
+}
